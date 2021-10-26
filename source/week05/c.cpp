0e25ed3bf663db781867b9ee687ff8b622b9a871
TasnadiAttila <70896019+TasnadiAttila@users.noreply.github.com>
2021-10-26 18:19:14 +0200

    Kiiír és Kilép

--- source/week05/c.cpp
+++ source/week05/c.cpp
@@ -75,8 +75,8 @@ Token Token_stream::get()
     cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
 
     switch (ch) {
-    case ';':    // for "print"
-    case 'q':    // for "quit"
+    case '=':    // for "print"
+    case 'x':    // for "quit"
     case '(': case ')': case '+': case '-': case '*': case '/':
         return Token(ch);        // let each character represent itself
     case '.':
